00553dab5f1d52201f8794d383e880e87d647935
LiftoffSBC <eagillet@gmail.com>
2018-06-15 09:59:49 -0400

    update

---
+++ assets/javascript/train.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDTQ0kxESzUq_hMvOXUqb8WqZkLpkCw8I",
+    authDomain: "train-tracker-acd90.firebaseapp.com",
+    databaseURL: "https://train-tracker-acd90.firebaseio.com",
+    projectId: "train-tracker-acd90",
+    storageBucket: "",
+    messagingSenderId: "397767617099"
+  };
+  firebase.initializeApp(config);
